79919adca4abb33b03f7fbccfcf67ddbec9dd4f9
aaphane <andriesap@dev.azure.com>
2020-11-01 21:50:13 +0200

    almost there

--- src/atmospheric_conditions/WeatherProvider.java
+++ src/atmospheric_conditions/WeatherProvider.java
@@ -5,18 +5,19 @@ import aerodyne.Coordinates;
 public class WeatherProvider {
 	
 	protected static WeatherProvider weatherProvider;
-	private static String[] weather;
+	private static String[] weather = {"FOG", "RAIN", "SUN", "SNOW"};
 	
 	private WeatherProvider() {
 		
 	}
 	
 	public static WeatherProvider getProvider() {
-		
+		weatherProvider = new WeatherProvider();
+        return weatherProvider;
 	}
 	
 	public String getCurrentWeather (Coordinates coordinates) {
-		
+        return weather[(coordinates.getHeight() + coordinates.getLatitude() + coordinates.getLongitude()) % 4];
 	}
 
 }
--- src/atmospheric_conditions/WeatherTower.java
+++ src/atmospheric_conditions/WeatherTower.java
@@ -1,14 +1,24 @@
 package atmospheric_conditions;
 
+
+
+import java.io.*; 
+
+
 import aerodyne.Coordinates;
 
 public class WeatherTower extends Tower {
 	
 	public String getWeather( Coordinates coordinates) {
-		
+        return WeatherProvider.getProvider().getCurrentWeather(coordinates);
 	}
 	
 	public void changeWeather() {
-		
+		try {
+            this.conditionsChanged();
+        }
+		catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 }
